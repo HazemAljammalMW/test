aed43c5c16ca7553a6cbc2f5a35aab13bb27a569
HazemAljammalMW <hazem.aljammal@mediaworldiq.com>
2025-05-15 15:08:51 +0300

    inital

---
+++ app/pages/child/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import dynamic from 'next/dynamic'
+
+const QAWidget = dynamic(
+    () => import('@gigit-ai/gigit-apps').then((module) => module.QAWidget),
+    {
+        ssr: false,
+    }
+)
+export function Child() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Child Page</h1>
+      <p>This is a child page.</p>
+        <QAWidget shop="miraaya.com" />
+
+    </div>
+  );
+}
