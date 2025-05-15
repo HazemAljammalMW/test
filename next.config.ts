5158e539af529b8dbc725a3ae5b70108bce65739
HazemAljammalMW <hazem.aljammal@mediaworldiq.com>
2025-05-15 15:31:19 +0300

    another

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ['@gigit-ai/gigit-apps'],
 };
 
 export default nextConfig;
