a42cac3f27c2c7b2cb58d4f5a8c246f855cb8176
HazemAljammalMW <hazem.aljammal@mediaworldiq.com>
2025-05-17 10:00:30 +0300

    test

--- app/pages/child/page.tsx
+++ app/pages/child/page.tsx
@@ -1,18 +1,24 @@
 "use client";
 import dynamic from 'next/dynamic'
 
-const QAWidget = dynamic(
-    () => import('@gigit-ai/gigit-apps').then((module) => module.QAWidget),
-    {
-        ssr: false,
-    }
-)
+// const QAWidget = dynamic(
+//     () => import('@gigit-ai/gigit-apps').then((module) => module.QAWidget),
+//     {
+//         ssr: false,
+//     }
+// )
+
+const Button = dynamic(
+  () => import("@/components/ui/button").then((mod) => mod.Button),
+  { ssr: false }
+);
+
 export function Child() {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl font-bold">Child Page</h1>
       <p>This is a child page.</p>
-        <QAWidget shop="miraaya.com" />
+      <Button>Click me</Button>
 
     </div>
   );
